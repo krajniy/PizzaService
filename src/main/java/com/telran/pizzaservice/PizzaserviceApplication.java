4d1bf4cdcf78bb4b1ba46dd3c5aa7ac30ffd1c7f
krajniy <krajniy@gmail.com>
2023-03-11 14:15:29 +0100

    init commit

---
+++ src/main/java/com/telran/pizzaservice/PizzaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.telran.pizzaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telran/pizzaservice/PizzaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telran.pizzaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
